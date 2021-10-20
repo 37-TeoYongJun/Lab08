dce006287b5f420a851315e28bd4a21bab0361d4
37-TeoYongJun <yongjun447@gmail.com>
2021-10-20 19:17:11 +0800

    Restart scene added
    just lose :D

--- Assets/Scripts/ObstacleMovement.cs
+++ Assets/Scripts/ObstacleMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ObstacleMovement : MonoBehaviour
 {
     private float xSpeed = -4f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +16,14 @@ public class ObstacleMovement : MonoBehaviour
     void Update()
     {
         transform.Translate(new Vector3(xSpeed*Time.deltaTime, 0, 0f));
-        if (transform.position.y < -5)
+        if (transform.position.x < -10)
         {
             Destroy(this.gameObject);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         float verticalInput = Input.GetAxis("Vertical");
 
         transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
